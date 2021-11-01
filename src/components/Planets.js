2e705f232cb0995b44a2948b5001e6bce40cfb07
Mhayk Whandson <hi@mhayk.com>
2021-11-01 16:46:50 +0000

    Planets component is using react-query to fetch planets endpoint.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
 
-// import { Container } from './styles';
+import api from '../services/api';
+
+const fetchPlanets = async () => {
+    const response = await api.get('/planets');
+    return response.data;
+}
 
 function Planets() {
-    return <div>Planets</div>
+    const { data, status } = useQuery('planets', fetchPlanets);
+
+    console.log(data);
+
+    return <div>
+        <h2>Planets</h2>
+        {/* <p>{status}</p> */}
+        {status === 'error' && (
+            <div>Error fetching data</div>
+        )}
+
+        {status === 'loading' && (
+            <div>Loading data...</div>
+        )}
+
+        {status === 'success' && (
+            <div>
+                {data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
+            </div>
+        )}
+    </div>
 }
 
 export default Planets;

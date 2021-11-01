eeb583c3df4b6e6456201844d0967c1e3d500053
Mhayk Whandson <hi@mhayk.com>
2021-11-01 16:49:23 +0000

    QueryClient Provider must be imported to use react-query version 3.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
 
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+
 function App() {
   const [page, setPage] = useState("planets");
 
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
+    <QueryClientProvider client={new QueryClient()}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
 
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={true} />
+
+    </QueryClientProvider>
   );
 }
 

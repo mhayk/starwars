49db15365bc0431934f24483c6ef04ef45a18781
Mhayk Whandson <hi@mhayk.com>
2021-11-01 16:46:15 +0000

    People component is using react-query to fetch people endpoint.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
-// import { Container } from './styles';
+import Person from './Person';
+
+import api from '../services/api';
+
+const fetchPeople = async () => {
+    const response = await api.get('/people');
+    return response.data;
+}
 
 function People() {
-    return <div>People</div>;
+    const { data, status } = useQuery('people', fetchPeople);
+
+    console.log(data);
+
+    return <div>
+        <h2>People</h2>
+        {status === 'error' && (
+            <div>Error fetching data</div>
+        )}
+
+        {status === 'loading' && (
+            <div>Loading data...</div>
+        )}
+
+        {status === 'success' && (
+            <div>
+                {data.results.map(person => <Person key={person.name} person={person} />)}
+            </div>
+        )}
+    </div>
 }
 
 export default People;
